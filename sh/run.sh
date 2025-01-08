36ed85adf37ac80281a5c5c4f2d1b989c00cb2fc
Kadir <abdcelikkanat@gmail.com>
2025-01-08 09:49:12 +0100

    negativeSamplesV2MoreNegatives2K

--- sh/run.sh
+++ sh/run.sh
@@ -15,7 +15,7 @@
 # Exit on first error and if any variables are unset
 #set -eu
 
-TEST_NAME="negativeSamplesV2NoMaxForNegSamples"
+TEST_NAME="negativeSamplesV2MoreNegatives2K"
 
 THREADS=40
 SEQ_MIN_LEN=3000
