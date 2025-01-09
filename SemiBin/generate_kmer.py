3a09a04585c51c49c0723de22cf32795c9fc1e77
Kadir <abdcelikkanat@gmail.com>
2025-01-09 10:25:18 +0100

    negativeSamplesV2TOnePosPairsEpochs50

--- SemiBin/generate_kmer.py
+++ SemiBin/generate_kmer.py
@@ -27,13 +27,9 @@ def generate_kmer_features_from_fasta(
             if not split:
                 yield h, seq
             elif len(seq) >= split_threshold:
-                onethird = len(seq) // 3
-                yield (h + '_a_1', seq[:1*onethird])
-                yield (h + '_a_2', seq[1 * onethird:2 * onethird])
-                yield (h + '_b_1', seq[:1 * onethird])
-                yield (h + '_b_2', seq[2 * onethird:3 * onethird])
-                yield (h + '_c_1', seq[1 * onethird:2 * onethird])
-                yield (h + '_c_2', seq[2 * onethird:3 * onethird])
+                onehalf = len(seq) // 2
+                yield (h + '_a_1', seq[:1*onehalf])
+                yield (h + '_a_2', seq[1 * onehalf:])
 
     kmer_dict, nr_features = generate_feature_mapping(kmer_len)
     composition = OrderedDict()
--- sh/run.sh
+++ sh/run.sh
@@ -15,7 +15,7 @@
 # Exit on first error and if any variables are unset
 #set -eu
 
-TEST_NAME="negativeSamplesV2ThreePosPairsEpochs50"
+TEST_NAME="negativeSamplesV2TOnePosPairsEpochs50"
 
 THREADS=40
 SEQ_MIN_LEN=3000
