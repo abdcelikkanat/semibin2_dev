c0febaa7d9be898fc54afd709617b1a65a4c5b4d
Kadir <abdcelikkanat@gmail.com>
2025-01-10 13:44:55 +0100

    TEST_GPU

--- sh/run.sh
+++ sh/run.sh
@@ -5,9 +5,10 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --partition=high-mem
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=10
-#SBATCH --mem=256G
+#SBATCH --mem=128G
 #SBATCH --time=3-00:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=abce@cs.aau.dk
@@ -15,11 +16,11 @@
 # Exit on first error and if any variables are unset
 #set -eu
 
-TEST_NAME="negativeSamplesV2TOnePosPairsEpochs50NoBound"
+TEST_NAME="TEST_GPU" #"negativeSamplesV2TOnePosPairsEpochs50NoBound"
 
 THREADS=40
 SEQ_MIN_LEN=3000
-SAMPLES=(PaPr00000216MP ASYM00000026MP) #(PaPr00000216MP RASK00000062MP ASYM00000026MP RASK00000098MP)
+SAMPLES=( PaPr00000216MP ) #(PaPr00000216MP ASYM00000026MP) #(PaPr00000216MP RASK00000062MP ASYM00000026MP RASK00000098MP)
 
 PROJECT_FOLDER="/home/cs.aau.dk/zs74qz/workspace/semibin2_dev"
 SEMIBIN2_PATH="/home/cs.aau.dk/zs74qz/.conda/envs/semibin2_dev/bin/SemiBin2"
