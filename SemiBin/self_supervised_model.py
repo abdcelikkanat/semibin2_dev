6a7a6e4f17063605a176077da80f5d2f8fe23b89
abdcelikkanat <abdcelikkanat@gmail.com>
2025-01-12 00:07:04 +0100

    SigmoidV4Epoch150PairBatch8K

--- SemiBin/self_supervised_model.py
+++ SemiBin/self_supervised_model.py
@@ -27,7 +27,7 @@ def train_self(logger, out : str, datapaths, data_splits, is_combined=True,
     ----------
     out : filename to write model to
     """
-    epoches = 50
+    epoches = 150
     batchsize = 8192
     loss_list = []
     from tqdm import tqdm
