d46caa97d7a68e9a3c9b63e91eb02d1f0c03c535
abdcelikkanat <abdcelikkanat@gmail.com>
2025-01-12 15:03:51 +0100

    SigmoidV4Epoch150PairsAndOneBatch8kLr1e3

--- sh/run.sh
+++ sh/run.sh
@@ -16,7 +16,7 @@
 # Exit on first error and if any variables are unset
 #set -eu
 
-TEST_NAME="SigmoidV4Epoch50PairsAndOneBatch8kLr1e3" #"negativeSamplesV2TOnePosPairsEpochs50NoBound"
+TEST_NAME="SigmoidV4Epoch150PairsAndOneBatch8kLr1e3" #"negativeSamplesV2TOnePosPairsEpochs50NoBound"
 
 THREADS=40
 SEQ_MIN_LEN=3000
