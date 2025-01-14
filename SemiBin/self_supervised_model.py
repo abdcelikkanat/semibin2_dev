715bf809b09783f621b44690630c886aa16ba442
abdcelikkanat <abdcelikkanat@gmail.com>
2025-01-15 00:28:12 +0100

    V5Bound4MBatch2KCosineLoss01LossLR1e2

--- SemiBin/self_supervised_model.py
+++ SemiBin/self_supervised_model.py
@@ -41,7 +41,7 @@ def train_self(logger, out : str, datapaths, data_splits, is_combined=True,
     out : filename to write model to
     """
     epoches = 50
-    learning_rate = 1e-3
+    learning_rate = 1e-2
     batchsize = 1024 * 2
     upper_bound = 4_000_000
     from tqdm import tqdm
