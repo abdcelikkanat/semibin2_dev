72734cbe631d63bd9811c9c53fecf01bf22bc487
abdcelikkanat <abdcelikkanat@gmail.com>
2025-01-16 12:21:54 +0100

    V6Bound4MBatch2KEpoch50LR1e3NoResnetFIX

--- SemiBin/semi_supervised_model.py
+++ SemiBin/semi_supervised_model.py
@@ -104,16 +104,17 @@ class Semi_encoding_single(torch.nn.Module):
 
     def encoder1(self, x0):
         x1 = self.linear1(x0)
-        x1 = self.dropout1(x1)
         x1 = self.batchnorm1(x1)
         x1 = self.sigmoid1(x1)
+        x1 = self.dropout1(x1)
 
-        x2 = self.linear1(x1)
-        x2 = self.dropout1(x2)
-        x2 = self.batchnorm1(x2)
-        x2 = self.sigmoid1(x2)
+        x2 = self.linear2(x1)
+        x2 = self.batchnorm2(x2)
+        x2 = self.sigmoid2(x2)
+        x2 = self.dropout2(x2)
 
-        return self.linear3(x2)
+        x3 = self.linear3(x2)
+        return x3
 
 
     def forward(self, input1, input2):
