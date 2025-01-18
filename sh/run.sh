dec4a3516e661bea8543576d0394399a48ba99dc
abdcelikkanat <abdcelikkanat@gmail.com>
2025-01-18 13:38:19 +0100

    V9K256mersEpoch150

--- sh/run.sh
+++ sh/run.sh
@@ -16,7 +16,7 @@
 # Exit on first error and if any variables are unset
 #set -eu
 
-TEST_NAME="V9K256mersEpoch50FIXED" #"negativeSamplesV2TOnePosPairsEpochs50NoBound"
+TEST_NAME="V9K256mersEpoch150" #"negativeSamplesV2TOnePosPairsEpochs50NoBound"
 
 THREADS=40
 SEQ_MIN_LEN=3000
